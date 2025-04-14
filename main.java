50c1ae647894ab9037cdbffd34420c708490c98c
shivangirai0922 <raishivangi943@gmail.com>
2025-04-14 19:11:10 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class main{
+    public static void main(String[] args) {
+        System.out.println("this is java program");
+        System.out.println("output from java");
+        System.out.println(new Date());
+    }
+}
+
+
+
