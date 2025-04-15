d202bac4819b8a112d0ce141f0c85173cc76d0c4
shivangirai0922 <raishivangi943@gmail.com>
2025-04-15 17:34:43 +0530

    add student class

--- main.java
+++ main.java
@@ -4,9 +4,14 @@ public class main{
     public static void main(String[] args) {
         System.out.println("this is java program");
         System.out.println("output from java");
-        System.out.println(new Date());
-        System.out.println("hello git");
+        System.out.println(new Date()); 
         System.out.println("added one more file");
+        student student=new student();
+        student.sayHello();
+
+
+
+    
     }
 }
 
---
+++ student.java
@@ -0,0 +1,8 @@
+public class student{
+    public student(){
+        System.out.println("creating student object");
+    }
+    public void sayHello() {
+        System.out.println("HI,I AM SHIVI");
+    }
+}
